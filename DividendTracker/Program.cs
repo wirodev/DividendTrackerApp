8d8d28ff8ba61878b9871547a7dcee758be79414
Robin Wiman <X00204927@myTUDublin.ie>
2024-04-16 22:12:00 +0100

    Updated Azure Connection string

--- DividendTracker/Program.cs
+++ DividendTracker/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
 
 var app = builder.Build();
 
